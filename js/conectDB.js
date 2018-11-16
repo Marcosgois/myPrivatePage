83328087a39be516e2ea3594a5f5bf414776e6df
José Marcos Vinícius Andrade Gois <goisinho_5@hotmail.com>
2018-11-16 13:01:04 -0200

    Update

--- js/conectDB.js
+++ js/conectDB.js
@@ -1,13 +1,34 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
+var Ator
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
-  database: "smdb"
+  password: "",
+  database: "projeto_hollywood",
+  port: 3306
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+con.query("SELECT nomeAtor, idAtor FROM Ator", function (err, result, fields) {
+  if (err) throw err;
+  //console.log(result);
+  Object.keys(result).forEach(function(key) {
+      var ator = result[key];
+      console.log(ator.nomeAtor, ator.idAtor)
+    });
+});
+
+con.query("SELECT nomePais FROM Pais", function (err, result, fields) {
+  if (err) throw err;
+  //console.log(result);
+  Object.keys(result).forEach(function(key) {
+      var pais = result[key];
+      console.log(pais.nomePais)
+    });
+});
+con.end();
